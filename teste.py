895689eccb625e76cf03fa6df4244e9214999be0
Eliel de Souza Faria <elielrj@gmail.com>
2024-11-04 11:18:02 -0300

    Inclusão de testes

---
+++ teste.py
@@ -0,0 +1,10 @@
+
+vizinhoMaisProximo = VizinhoMaisProximo()
+
+sequencia = [0,1,3,2,9,1,14,15,1,2,2,10,7]
+
+vizinhoMaisProximo.incluir_sequencia(sequencia)
+
+vizinhoMaisProximo.buscar_vetor_do_vizinho_mais_proximo()
+
+
